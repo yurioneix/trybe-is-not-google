863ec697680a733b6798dce694e42c34f0a34389
yurioneix <yurioneix18@gmail.com>
2023-07-14 18:23:33 -0300

    Requisito 2: resolvido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        print("Formato inválido", file=sys.stderr)
+        # raise ValueError("Formato inválido")
+    try:
+        with open(path_file, "r") as file:
+            text = file.read()
+            text_list = text.split("\n")
+            return text_list
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
