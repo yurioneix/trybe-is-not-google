c703527dd0a6d978ceb1c8c5c67ed34835f3fd57
yurioneix <yurioneix18@gmail.com>
2023-07-14 15:36:41 -0300

    Requisito 1: implementa fila para armazenar arquivos

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._queue) == 0:
+            return None
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self._queue):
+            return self._queue[index]
+        raise IndexError("Índice Inválido ou Inexistente")
