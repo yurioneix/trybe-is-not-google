e4c11b6d89c767ca152cbce1ac19ff21a7d72af3
yurioneix <yurioneix18@gmail.com>
2023-07-17 17:39:59 -0300

    Requisito 6: implementa testes da PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,20 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    new_priority_queue = PriorityQueue()
+    new_priority_queue.enqueue({"qtd_linhas": 2})
+
+    assert len(new_priority_queue) == 1
+
+    new_priority_queue.enqueue({"qtd_linhas": 6})
+    assert len(new_priority_queue) == 2
+
+    assert new_priority_queue.dequeue() == {"qtd_linhas": 2}
+
+    new_priority_queue.enqueue({"qtd_linhas": 1})
+    assert new_priority_queue.search(0) == {"qtd_linhas": 1}
+
+    with pytest.raises(IndexError):
+        new_priority_queue.search(404)
