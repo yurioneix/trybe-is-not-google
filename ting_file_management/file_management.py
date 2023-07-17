426733fff7080ddddeb2bcce7ee7d02300e4d3ac
yurioneix <yurioneix18@gmail.com>
2023-07-17 15:13:34 -0300

    Requisito 3: implementa funcao file_process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -4,7 +4,6 @@ import sys
 def txt_importer(path_file):
     if ".txt" not in path_file:
         print("Formato inválido", file=sys.stderr)
-        # raise ValueError("Formato inválido")
     try:
         with open(path_file, "r") as file:
             text = file.read()
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    new_list = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(new_list),
+        "linhas_do_arquivo": new_list,
+        }
+    for dict in instance._queue:
+        if path_file == dict["nome_do_arquivo"]:
+            return
+    instance.enqueue(new_dict)
+    print(new_dict)
 
 
 def remove(instance):
